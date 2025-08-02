ee127d0d95926c5c61e1256971dbd996965e29e3
Bruce Nielson <brucenielson1@gmail.com>
2025-08-02 11:16:34 -0600

    Simplifying AI Philosopher

--- check_encodings.py
+++
@@ -1,8 +0,0 @@
-import locale
-import sys
-
-# Check system's preferred encoding
-print("Preferred encoding:", locale.getpreferredencoding(False))
-
-# Check Python's stdout encoding
-print("Python stdout encoding:", sys.stdout.encoding)
